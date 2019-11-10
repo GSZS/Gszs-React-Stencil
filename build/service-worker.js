4f2e0cd7e4b1006a12ff7e0e9d41d62223864ca2
GSZS <xhg1945454120@gmail.com>
2019-11-10 13:32:27 +0800

    完善简易的部署文档

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f201734c2043cec3fa7aa19dac6648f4.js"
+  "./precache-manifest.c2da84dc85a3f441d1255973ad25f25a.js"
 );
 
 workbox.clientsClaim();
